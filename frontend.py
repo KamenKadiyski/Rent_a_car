9b6d8de7a0e732041df46bc13278e6e1ee5c1937
KamenKadiyski <kamen.kadiyski@gmail.com>
2024-10-29 12:09:53 +0000

    Коригирани бъгове и добавени липсващи проверки

--- frontend.py
+++ frontend.py
@@ -49,14 +49,7 @@ def exit_menu():
     except Exception as e:
         print(f"Error: {e}")
 
-       # with open("rented_cars.json", "w") as f1:
-            #json.dump(transactions, f1, indent=4)
-        #with open("cars_data.json", "w") as f2:
-          #  json.dump(cars, f2, indent=4)
-        #with open("clients_data.json", "w",encoding= "utf-8") as f3:
-          #  json.dump(client, f3,ensure_ascii= False ,indent=4)
-    #except Exception as e:
-        #print(f"Error: {e}")
+
     return None
 
 
