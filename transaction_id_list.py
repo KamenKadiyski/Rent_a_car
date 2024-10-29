b4fcb0080f34ad6cd4cbdb1e465b660347af5fb9
KamenKadiyski <kamen.kadiyski@gmail.com>
2024-10-29 12:36:14 +0000

    Коригирани бъгове и добавени липсващи проверки

--- transaction_id_list.py
+++ transaction_id_list.py
@@ -7,3 +7,9 @@ def id_generate(car_id, client_id) -> str:
     trans_list.append(day_of_rent)
     trans_id = ':'.join(trans_list)
     return trans_id
+def info_from_id(trans_id):
+    trans_list = trans_id.split(':')
+    car_id = trans_list[0]
+    client_id = trans_list[1]
+    day_of_rent = trans_list[2]
+    return car_id, client_id, day_of_rent
